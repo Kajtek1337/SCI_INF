294c833c7dbc79209dc457ea1b3fef11996dc6a7
kajtek <kajtek@DOM.SCI.EDU.PL>
2021-11-02 16:35:16 +0100

    update2

--- PracaNaLekcji1/szyfrowanie.cpp
+++ PracaNaLekcji1/szyfrowanie.cpp
@@ -1,20 +1,45 @@
 ﻿
 
+
 #include <iostream>
 #include <string>
 using namespace std;
 
+string szyfrcezara(string zdanie)
+{
+	int przesuniecie;
+	cout << "o ile przesuwamy" << endl;
+	cin >> przesuniecie;
+	cout << endl;
+	for (int i = 0; i < zdanie.size(); i++)
+	{
+		if (zdanie[i] < 123 && zdanie[i] > 96)
+		{
+			zdanie[i] += przesuniecie;
+
+		}
+	}
+	return zdanie;
+}
+
+
 int main()
 {
-	string liczba;
-	cout << "podaj o ile przesuwamy" << endl;  
-	cin >> liczba;
-	getline(cin,liczba); 
+	string zdanie;
+	int sposob;
+	cout << "napisz zdanie/slowo" << endl;
+
+	getline(cin, zdanie);
 	cout << "sposób szyfrowania (1-4)" << endl;
 	cout << "1. szyfr cezara" << endl;
 	cout << "2. szyf przestawieniowy" << endl;
 	cout << "3. szyf cezara i przestawieniowy" << endl;
-	cout << "4. Odzszyfrowywanie" << endl; 
+	cout << "4. Odzszyfrowywanie" << endl;
+	cin >> sposob;
+
+	cout << "Szyfr to: " << szyfrcezara(zdanie) << endl;
 
 }
+
+
 
