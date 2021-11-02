9421ea93e84b532d5967484633c59cca90bffc99
kajtek <kajtek@DOM.SCI.EDU.PL>
2021-11-02 15:51:41 +0100

    update

---
+++ PracaNaLekcji1/szyfrowanie.cpp
@@ -0,0 +1,20 @@
+﻿
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string liczba;
+	cout << "podaj o ile przesuwamy" << endl;  
+	cin >> liczba;
+	getline(cin,liczba); 
+	cout << "sposób szyfrowania (1-4)" << endl;
+	cout << "1. szyfr cezara" << endl;
+	cout << "2. szyf przestawieniowy" << endl;
+	cout << "3. szyf cezara i przestawieniowy" << endl;
+	cout << "4. Odzszyfrowywanie" << endl; 
+
+}
+
